5e2655c9d98fbfb60a2a23ab78062db76542bf25
Opilane TTHK <opilane@tthk.ee>
2020-09-01 13:20:44 +0300

    My First App

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //this is a commentary
+            //Console.WriteLine("Minecraft2!");
+            Console.WriteLine("What's Your Name?");
+            string Name;
+            //read the input from the user
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello," + Name);
+
+        }
+    }
+}
